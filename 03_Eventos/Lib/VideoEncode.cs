3ba18a239d4c4b62f0d95f90f6e1489c049b6d97
Heber Pietrafeza <hpietrafeza@autodoc.com.br>
2019-04-09 13:04:49 -0300

    events

---
+++ 03_Eventos/Lib/VideoEncode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _03_Eventos.Lib
+{
+    public class VideoEncode
+    {
+        //public delegate void VideoEncodedHandler(Video video);
+        //public event VideoEncodedHandler Encoded;
+        public event EventHandler<VideoEventArgs> Encoded;
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Convertendo video...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Video Convertido");
+
+            Encoded(this, new VideoEventArgs() { Video = video});
+        }
+    }
+
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+    }
+}
