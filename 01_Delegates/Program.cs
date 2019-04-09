3a40645346e4cc35193bae37d27b660529435f4a
Heber Pietrafeza <hpietrafeza@autodoc.com.br>
2019-04-09 11:43:01 -0300

    delegates exemplos iniciais

---
+++ 01_Delegates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Delegates
+{
+    class Program
+    {
+        delegate int Calcula(int a, int b);
+        static void Main(string[] args)
+        {
+            Calcula calcSoma = new Calcula(Soma);
+            var so = calcSoma(10, 20);
+
+            Calcula calcSub = new Calcula(Subtrai);
+            var su = calcSub(20, 10);
+
+            //var so = Soma(10, 20);
+
+            //var su = Subtrai(20, 10);
+
+            Console.WriteLine("Soma:" + so);
+            Console.WriteLine("Sub:" + su);
+        }
+
+        static int Soma(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Subtrai(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ 02_Delegates/Lib/FotoProcessador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Delegates.Lib
+{
+    public class FotoProcessador
+    {
+        //Handler palavra comum para identificar delegates
+        public delegate void FotoFiltroHandler(Foto foto);
+
+        public static FotoFiltroHandler filtros;
+
+        public static void Processar(Foto foto)
+        {
+            filtros(foto);
+        }
+
+        //Aplica todos os efeitos de uma vez
+        public static void ProcessarTodos(Foto foto)
+        {
+            FotoFiltro filtros = new FotoFiltro();
+            filtros.Colorir(foto);
+            filtros.PretoBranco(foto);
+            filtros.GerarThumb(foto);
+            filtros.RedimensionarTamMedio(foto);
+        }
+    }
+}
